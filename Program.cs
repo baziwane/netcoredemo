815204874345394cc22c72dd75298776b72bb454
Raymond B Matovu <raymond.baziwane@gmail.com>
2017-04-27 20:44:03 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
