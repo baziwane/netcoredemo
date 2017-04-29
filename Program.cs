a9d2586ea5d97c2a1c88c0933fb51ec4c35c09b1
Raymond B Matovu <raymond.baziwane@gmail.com>
2017-04-28 19:43:46 -0500

    changed message

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace NetCoreDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You are one in a million!");
+            Console.WriteLine("You are one in a million bucks!");
         }
     }
 }
